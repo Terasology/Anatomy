6c845856659448740e52c28c8aa5855fe364c84f
XTariq <xrtariq@gmail.com>
2016-05-23 16:42:09 -0700

    Removed extraneous file.

--- src/main/java/org/terasology/anatomy/AnatomyPart.java
+++ src/main/java/org/terasology/anatomy/AnatomyPart.java
@@ -15,8 +15,6 @@
  */
 package org.terasology.anatomy;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.network.Replicate;
 
 /**
@@ -24,9 +22,6 @@ import org.terasology.network.Replicate;
  * HandsComponent, etc.
  */
 public class AnatomyPart {
-
-    private static final Logger logger = LoggerFactory.getLogger(AnatomySystem.class);
-
     /**
      * Used to determine if this limb needs to be "revived".
      */
