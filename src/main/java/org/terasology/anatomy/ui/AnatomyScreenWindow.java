720ead9accc4ff8455233e70c70830115eda5af8
Arpan Banerjee <arpanbnrj9@gmail.com>
2017-06-17 08:01:28 +0530

    added tooltip

--- src/main/java/org/terasology/anatomy/ui/AnatomyScreenWindow.java
+++ src/main/java/org/terasology/anatomy/ui/AnatomyScreenWindow.java
@@ -17,18 +17,103 @@ package org.terasology.anatomy.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.terasology.anatomy.event.AnatomyStatusGatheringEvent;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.characters.CharacterComponent;
+import org.terasology.logic.players.LocalPlayer;
+import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.nui.BaseInteractionScreen;
+import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.rendering.nui.skin.UISkin;
+import org.terasology.rendering.nui.skin.UISkinBuilder;
+import org.terasology.rendering.nui.widgets.UILabel;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class AnatomyScreenWindow extends BaseInteractionScreen {
-    private static Logger logger = LoggerFactory.getLogger(AnatomyScreenWindow.class);
+    private static final String ANATOMY_PART_PREFIX = "Anatomy:";
+    private EntityRef player = EntityRef.NULL;
+
     @Override
     public void initialise() {
-        logger.info("initialise");
     }
 
     @Override
     protected void initializeWithInteractionTarget(EntityRef interactionTarget) {
-        logger.info("initialise with " + interactionTarget.toFullDescription());
+    }
+
+    private void reInit() {
+        player = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
+
+        // In case the player has been created yet, exit out early to prevent an error.
+        if (player == EntityRef.NULL) {
+            return;
+        }
+    }
+
+    @Override
+    public void onOpened() {
+        EntityRef characterEntity = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
+        CharacterComponent characterComponent = characterEntity.getComponent(CharacterComponent.class);
+
+        // In case the player has been created yet, exit out early to prevent an error.
+        if (characterComponent == null) {
+            return;
+        }
+
+        // If the reference to the player entity hasn't been set yet, or it refers to a NULL entity, call the reInit()
+        // method to set it. The getId() check is necessary for certain network entities whose ID is 0, but are
+        // erroneously marked as existent.
+        if (!player.exists() || (player.exists() && (player == EntityRef.NULL || player.getId() == 0 || player == null))) {
+            reInit();
+        }
+
+        // As long as there's an interaction target, open this window.
+        if (getInteractionTarget() != EntityRef.NULL) {
+            initializeWithInteractionTarget(getInteractionTarget());
+            super.onOpened();
+        }
+
+        // Every time the character screen window is opened, update the Anatomy part statuses.
+        updateStatuses();
+    }
+
+    private void updateStatuses() {
+        // Only update the statuses if the player character entity actually exists.
+        if (player == null || player == EntityRef.NULL || player.getId() == 0) {
+            return;
+        }
+
+        AnatomyStatusGatheringEvent event = new AnatomyStatusGatheringEvent();
+        player.send(event);
+        Map<String, List<String>> partEffectsMap = event.getEffectsMap();
+
+        Collection<UILabel> labels = findAll(UILabel.class);
+        for (UILabel label : labels) {
+            if (label.getId().contains(ANATOMY_PART_PREFIX)) {
+                String partID = label.getId().substring(ANATOMY_PART_PREFIX.length());
+                List<String> partEffects = partEffectsMap.get(partID);
+                if (partEffects == null) {
+                    // No effects for this part
+                    label.bindTooltipString(new ReadOnlyBinding<String>() {
+                        @Override
+                        public String get() {
+                            return null;
+                        }
+                    });
+                } else {
+                    // This part has effects
+                    label.setTooltipDelay(0);
+                    label.bindTooltipString(new ReadOnlyBinding<String>() {
+                        @Override
+                        public String get() {
+                            return String.join(",", partEffects);
+                        }
+                    });
+                }
+            }
+        }
     }
 }
