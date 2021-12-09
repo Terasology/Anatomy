df92c82bcfe8e2d9ec75bcc1c276affde33ff8f0
darkweird <foxflameless@gmail.com>
2021-12-09 10:41:01 +0300

    fix(checkstyle): Fix checkstyle issues and update copyright

--- src/main/java/org/terasology/anatomy/AnatomyCirculation/CirculatoryHealthSystem.java
+++ src/main/java/org/terasology/anatomy/AnatomyCirculation/CirculatoryHealthSystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.anatomy.AnatomyCirculation;
 
 import org.terasology.anatomy.AnatomyCirculation.component.InjuredCirculatoryComponent;
@@ -38,25 +25,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RegisterSystem(value = RegisterMode.AUTHORITY)
+@RegisterSystem(RegisterMode.AUTHORITY)
 public class CirculatoryHealthSystem extends BaseComponentSystem {
+
+    private static final String CIRCULATORY_REGEN_PREFIX = "Circulatory:Regen:";
+    private static final String CIRCULATORY_BLOOD_REGEN_PREFIX = "Circulatory:BloodRegen:";
+    private static final String CIRCULATORY_CHARACTERISTIC = "blood";
+    private final Map<Integer, Float> severityBleedingRateMap = new HashMap<>();
+    private final float bluntDamageMultiplier = 0.5f;
+    private final float pierceDamageMultiplier = 1.5f;
+
     @In
     private Time time;
-
     @In
     private EntityManager entityManager;
-
     @In
     private DelayManager delayManager;
-
-    private Map<Integer, Float> severityBleedingRateMap = new HashMap<>();
-
-    private float bluntDamageMultiplier = 0.5f;
-    private float pierceDamageMultiplier = 1.5f;
-
-    private String CIRCULATORY_REGEN_PREFIX = "Circulatory:Regen:";
-    private String CIRCULATORY_BLOOD_REGEN_PREFIX = "Circulatory:BloodRegen:";
-    private String CIRCULATORY_CHARACTERISTIC = "blood";
 
     @Override
     public void initialise() {
@@ -66,23 +50,30 @@ public class CirculatoryHealthSystem extends BaseComponentSystem {
     }
 
     @ReceiveEvent
-    public void onBloodLevelRegen(DelayedActionTriggeredEvent event, EntityRef entityRef, InjuredCirculatoryComponent injuredCirculatoryComponent) {
+    public void onBloodLevelRegen(DelayedActionTriggeredEvent event, EntityRef entityRef,
+                                  InjuredCirculatoryComponent injuredCirculatoryComponent) {
         if (event.getActionId().startsWith(CIRCULATORY_BLOOD_REGEN_PREFIX)) {
-            if (injuredCirculatoryComponent.bloodLevel >= 0 && injuredCirculatoryComponent.bloodLevel <= injuredCirculatoryComponent.maxBloodLevel && injuredCirculatoryComponent.bloodRegenRate != 0) {
+            if (injuredCirculatoryComponent.bloodLevel >= 0
+                    && injuredCirculatoryComponent.bloodLevel <= injuredCirculatoryComponent.maxBloodLevel
+                    && injuredCirculatoryComponent.bloodRegenRate != 0) {
                 int healAmount = 0;
                 healAmount += injuredCirculatoryComponent.bloodRegenRate / TeraMath.fastAbs(injuredCirculatoryComponent.bloodRegenRate);
-                injuredCirculatoryComponent.nextRegenTick = time.getGameTimeInMs() + (long) (1000 / TeraMath.fastAbs(injuredCirculatoryComponent.bloodRegenRate));
+                injuredCirculatoryComponent.nextRegenTick =
+                        time.getGameTimeInMs() + (long) (1000 / TeraMath.fastAbs(injuredCirculatoryComponent.bloodRegenRate));
                 injuredCirculatoryComponent.bloodLevel += healAmount;
-                injuredCirculatoryComponent.bloodLevel = TeraMath.clamp(injuredCirculatoryComponent.bloodLevel, 0, injuredCirculatoryComponent.maxBloodLevel);
+                injuredCirculatoryComponent.bloodLevel = TeraMath.clamp(injuredCirculatoryComponent.bloodLevel, 0,
+                        injuredCirculatoryComponent.maxBloodLevel);
                 entityRef.saveComponent(injuredCirculatoryComponent);
                 entityRef.send(new BloodLevelChangedEvent());
             }
-            delayManager.addDelayedAction(entityRef, CIRCULATORY_BLOOD_REGEN_PREFIX, (long) (1000 / TeraMath.fastAbs(injuredCirculatoryComponent.bloodRegenRate)));
+            delayManager.addDelayedAction(entityRef, CIRCULATORY_BLOOD_REGEN_PREFIX,
+                    (long) (1000 / TeraMath.fastAbs(injuredCirculatoryComponent.bloodRegenRate)));
         }
     }
 
     @ReceiveEvent
-    public void onPartHealthRegen(DelayedActionTriggeredEvent event, EntityRef entityRef, InjuredCirculatoryComponent injuredCirculatoryComponent) {
+    public void onPartHealthRegen(DelayedActionTriggeredEvent event, EntityRef entityRef,
+                                  InjuredCirculatoryComponent injuredCirculatoryComponent) {
         if (event.getActionId().startsWith(CIRCULATORY_REGEN_PREFIX)) {
             String partID = event.getActionId().substring(CIRCULATORY_REGEN_PREFIX.length());
             PartHealthDetails partDetails = injuredCirculatoryComponent.partHealths.get(partID);
@@ -99,7 +90,8 @@ public class CirculatoryHealthSystem extends BaseComponentSystem {
     }
 
     @ReceiveEvent
-    public void onBleedingRateChanged(PartCirculatoryEffectChangedEvent event, EntityRef entityRef, InjuredCirculatoryComponent injuredCirculatoryComponent) {
+    public void onBleedingRateChanged(PartCirculatoryEffectChangedEvent event, EntityRef entityRef,
+                                      InjuredCirculatoryComponent injuredCirculatoryComponent) {
         float bloodRegenRate = injuredCirculatoryComponent.baseBloodRegenRate;
         for (Map.Entry<String, List<String>> severityPartsEntry : injuredCirculatoryComponent.parts.entrySet()) {
             bloodRegenRate += severityPartsEntry.getValue().size() * severityBleedingRateMap.get(Integer.parseInt(severityPartsEntry.getKey()));
@@ -121,8 +113,9 @@ public class CirculatoryHealthSystem extends BaseComponentSystem {
                 partHealthDetails = new PartHealthDetails();
                 injuredCirculatoryComponent.partHealths.put(event.getTargetPart().id, partHealthDetails);
                 // Part has been injured for the first time, so add delayed part health regen event and blood level regen event.
-                delayManager.addDelayedAction(entityRef, CIRCULATORY_REGEN_PREFIX + event.getTargetPart().id, (long) (1000 / partHealthDetails.regenRate));
-                delayManager.addDelayedAction(entityRef, CIRCULATORY_BLOOD_REGEN_PREFIX, (long) 1000);
+                delayManager.addDelayedAction(entityRef, CIRCULATORY_REGEN_PREFIX + event.getTargetPart().id,
+                        (long) (1000 / partHealthDetails.regenRate));
+                delayManager.addDelayedAction(entityRef, CIRCULATORY_BLOOD_REGEN_PREFIX, 1000);
             }
             int damageAmount = event.getAmount();
             if (event.getDamageType().getName().equals("Equipment:pierceDamage")) {
--- src/main/java/org/terasology/anatomy/event/AnatomyStatusGatheringEvent.java
+++ src/main/java/org/terasology/anatomy/event/AnatomyStatusGatheringEvent.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.anatomy.event;
 
 import com.google.common.collect.Lists;
@@ -34,7 +21,7 @@ public class AnatomyStatusGatheringEvent implements Event {
     /**
      * The map which stores the parts mapped to list of effects.
      */
-    private Map<String, List<String>> effectsMap = new HashMap<>();
+    private final Map<String, List<String>> effectsMap = new HashMap<>();
 
     /**
      * Create the event for all systems to respond to.
